1cc049e739e61ec703ac1dd38626f5699b7c0ce3
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-04-25 20:33:23 +0300

    added new html templates

--- app/views.py
+++ app/views.py
@@ -9,4 +9,22 @@ def index():
     View root page function that returns the index page and its data
     '''
     message = 'Hello World'
-    return render_template('index.html')
+    return render_template('index.html', message = message )
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+
+    return render_template ('news.html', id = news_id )
+
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Your Best News Source'
+    return render_template('index.html', title = title)
