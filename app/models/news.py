2494f98ff85c871a9a6380979bd6c031603bf57a
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-04-26 10:46:41 +0300

    added the function to get the news from the api

---
+++ app/models/news.py
@@ -0,0 +1,12 @@
+class News:
+    '''
+    News Class to define News Objects
+    '''
+
+    def __init__ (self, name, author,title, description,url, content):
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.content = content
--- app/request.py
+++ app/request.py
@@ -1,4 +1,30 @@
 from app import app
+import urllib.request,json
+from .models import news
+
+News = news.News
 
 # Getting api key
-api_key = app.config['MOVIE_API_KEY']
+api_key = app.config['NEWS_API_KEY']
+
+#Getting the News base url
+base_url = app.config["NEWS_API_BASE_URL"]
+
+def get_news(category):
+    '''
+    Function that gets the json response to out url request
+    '''
+
+    get_news_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_date = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['results']:
+            news_results_list = get_news_response['results']
+            new_results = process_results(news_results_list)
+
+    return news_results
